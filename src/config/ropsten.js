1ede184ab5963f8f0349ad22b17642a829aa56c1
Jirka Chadima <chadima.jiri@gmail.com>
2018-07-30 14:05:03 +0200

    chore: Changes the address of demo WTIndex on Ropsten

--- src/config/ropsten.js
+++ src/config/ropsten.js
@@ -4,7 +4,7 @@ const HttpAdapter = require('@windingtree/off-chain-adapter-http');
 const knex = require('knex');
 
 module.exports = {
-  wtIndexAddress: '0x407f550023eb6ad8a4797844489e17c5ced17e06',
+  wtIndexAddress: '0x933198455e38925bccb4bfe9fb59bac31d00b4d3',
   port: 8000,
   wtLibs: WTLibs.createInstance({
     dataModelOptions: {
