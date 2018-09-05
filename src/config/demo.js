143b6527efc3fbaf68d5eb8c6b7ac8351b93e85d
Hynek Urban <hynek.urban@fragaria.cz>
2018-09-05 08:55:28 +0200

    chore: Change swarm gateway in demo.

--- src/config/demo.js
+++ src/config/demo.js
@@ -4,7 +4,7 @@ module.exports = {
   port: 8000,
   wtIndexAddress: '0x933198455e38925bccb4bfe9fb59bac31d00b4d3',
   ethereumProvider: 'https://ropsten.infura.io/WKNyJ0kClh8Ao5LdmO7z',
-  swarmProvider: 'https://swarm-gateways.net',
+  swarmProvider: 'https://swarm.windingtree.com',
   db: knex({
     client: 'sqlite3',
     connection: {
