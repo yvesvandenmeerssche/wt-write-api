932e4fabfbb21c7132b165c189487c19e530aec6
Jirka Chadima <chadima.jiri@gmail.com>
2018-10-15 14:55:11 +0200

    feat(config): Moves INFURA_API_KEY into travis encrypted vars

--- src/config/demo.js
+++ src/config/demo.js
@@ -3,7 +3,7 @@ const knex = require('knex');
 module.exports = {
   port: 8000,
   wtIndexAddress: '0x933198455e38925bccb4bfe9fb59bac31d00b4d3',
-  ethereumProvider: 'https://ropsten.infura.io/WKNyJ0kClh8Ao5LdmO7z',
+  ethereumProvider: 'https://ropsten.infura.io/' + process.env.INFURA_API_KEY,
   swarm: {
     provider: 'https://swarm.windingtree.com',
     timeoutRead: 1000,
