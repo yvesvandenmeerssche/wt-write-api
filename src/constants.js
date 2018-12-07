7fc5201c0982b6a47f9524e4e175147ded6efe1d
Ondrej Chmelar <ondrej.chmelar@fragaria.cz>
2018-12-07 11:18:35 +0100

    improvement(model): wtip-003 bump data format version

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@ module.exports = {
   ACCESS_KEY_HEADER: 'X-Access-Key',
   WALLET_PASSWORD_HEADER: 'X-Wallet-Password',
   // Data format version to which all validations and data structuring conform.
-  DATA_FORMAT_VERSION: '0.1.0',
+  DATA_FORMAT_VERSION: '0.2.0',
 };
